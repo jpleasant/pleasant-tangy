e87a327ef8a60bdc55bdf6b2e249885f46dd89a8
Jeremy Pleasant <jeremy@trendsic.com>
2021-07-18 12:56:00 -0500

    Initial Commit

---
+++ plugin-manager-mobile/plugin-manager-mobile/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using plugin_manager_mobile.Models;
+using plugin_manager_mobile.ViewModels;
+using plugin_manager_mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace plugin_manager_mobile.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ plugin-manager-mobile/plugin-manager-mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using plugin_manager_mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace plugin_manager_mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
