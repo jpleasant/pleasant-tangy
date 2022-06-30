3f0610a969178b8eafe35f3907280c2e566d1166
Jeremy Pleasant <jeremy.pleasant@outlook.com>
2022-06-30 06:32:11 -0500

    category

---
+++ PluginManager_Mobile/PluginManager_Mobile/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PluginManager_Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace PluginManager_Mobile.ViewModels
+{
+    public class CategoryViewModel : BaseViewModel
+    {
+        public bool IsActive { get; set; }
+        public AsyncCommand<Category> CategorySelected { get; }
+
+        public CategoryViewModel()
+        {
+            IsActive = false;
+            CategorySelected = new AsyncCommand<Category>(OnCategorySelected);
+        }
+        async Task OnCategorySelected(Category c)
+        {
+            IsActive = true;
+        }
+    }
+}
--- PluginManager_Mobile/PluginManager_Mobile/Views/CategoryPage.xaml.cs
+++ PluginManager_Mobile/PluginManager_Mobile/Views/CategoryPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PluginManager_Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,13 @@ namespace PluginManager_Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CategoryPage : ContentPage
     {
+        private readonly CategoryViewModel _categoryVM;
         public CategoryPage()
         {
+            _categoryVM = new CategoryViewModel();
+            BindingContext = _categoryVM;
             InitializeComponent();
+
         }
     }
 }
